2fea279d42f38ac0365d16b87c61965dc3c036c7
mikolaj.leszczynski <mikolaj.leszczynski@babylonhealth.com>
2019-05-29 10:05:04 +0200

    Updated the kata

--- app/src/test/java/codekata/babylon/com/codekata/CheckoutTest.kt
+++ app/src/test/java/codekata/babylon/com/codekata/CheckoutTest.kt
@@ -6,29 +6,49 @@ import org.junit.Test
 
 class CheckoutTest {
 
-    @Test
-    fun total_isCorrect() {
-        assertEquals(0, price(""))
-        assertEquals(50, price("A"))
-        assertEquals(80, price("AB"))
-        assertEquals(115, price("CDBA"))
-
-        assertEquals(100, price("AA"))
-        assertEquals(130, price("AAA"))
-        assertEquals(180, price("AAAA"))
-        assertEquals(230, price("AAAAA"))
-        assertEquals(260, price("AAAAAA"))
-
-        assertEquals(160, price("AAAB"))
-        assertEquals(175, price("AAABB"))
-        assertEquals(190, price("AAABBD"))
-        assertEquals(190, price("DABABA"))
-    }
-
     private fun price(items: CharSequence): Int {
         val checkout: Checkout = mock()
         items.forEach { checkout.scan(it) }
         return checkout.total()
     }
+
+    @Test
+    fun total_isCorrect_for_empty_basket() = assertEquals(0, price(""))
+
+    @Test
+    fun total_isCorrect_for_A() = assertEquals(50, price("A"))
+
+    @Test
+    fun total_isCorrect_for_AB() = assertEquals(80, price("AB"))
+
+    @Test
+    fun total_isCorrect_for_CDBA() = assertEquals(115, price("CDBA"))
+
+    @Test
+    fun total_isCorrect_for_AA() = assertEquals(100, price("AA"))
+
+    @Test
+    fun total_isCorrect_for_AAA() = assertEquals(130, price("AAA"))
+
+    @Test
+    fun total_isCorrect_for_AAAA() = assertEquals(180, price("AAAA"))
+
+    @Test
+    fun total_isCorrect_for_AAAAA() = assertEquals(230, price("AAAAA"))
+
+    @Test
+    fun total_isCorrect_for_AAAAAA() = assertEquals(260, price("AAAAAA"))
+
+    @Test
+    fun total_isCorrect_for_AAAB() = assertEquals(160, price("AAAB"))
+
+    @Test
+    fun total_isCorrect_for_AAABB() = assertEquals(175, price("AAABB"))
+
+    @Test
+    fun total_isCorrect_for_AAABBD() = assertEquals(190, price("AAABBD"))
+
+    @Test
+    fun total_isCorrect_for_DABABA() = assertEquals(190, price("DABABA"))
 
 }
